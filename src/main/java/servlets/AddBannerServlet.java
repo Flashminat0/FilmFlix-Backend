5cf51595037d4af0ef12bf75eaa0795374888b19
Yasas Lowe <64888128+Flashminat0@users.noreply.github.com>
2021-05-06 19:31:48 +0530

    created multiple servlets for easier coding

---
+++ src/main/java/servlets/AddBannerServlet.java
@@ -0,0 +1,46 @@
+package servlets;
+
+import model.Banner;
+import model.Movie;
+import utils.BannerUtils;
+import utils.MovieUtils;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "AddBannerServlet", value = "/AddBannerServlet")
+public class AddBannerServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int movieid = Integer.parseInt(request.getParameter("movieid"));
+        String bannerlocation = request.getParameter("bannerlocation");
+        String moviename = request.getParameter("moviename");
+
+
+        Banner banner = new Banner();
+        banner.setMovieid(movieid);
+        banner.setBannerlocation(bannerlocation);
+
+        Movie movie = new Movie();
+        movie.setMovieid(movieid);
+        movie.setMoviename(moviename);
+
+        boolean statusAddBanner = BannerUtils.AddBanner(banner);
+        boolean statusAddMovie = MovieUtils.AddMovie(movie);
+
+        if (statusAddBanner && statusAddMovie) {
+            System.out.println("Sucess");
+            RequestDispatcher dispatcher = request.getRequestDispatcher("/jsp/movies.jsp");
+            dispatcher.forward(request, response);
+        } else {
+            System.out.println("waradi");
+        }
+    }
+}
---
+++ src/main/java/servlets/BannerServletForwarder.java
@@ -0,0 +1,38 @@
+package servlets;
+
+import model.Banner;
+import model.Movie;
+import utils.BannerUtils;
+import utils.MovieUtils;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "BannerServletCUD", value = "/BannerServletCUD")
+public class BannerServletForwarder extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+        if (request.getParameter("create-btn") != null) {
+            RequestDispatcher dispatcher = request.getRequestDispatcher("/jsp/CreateBanner");
+            dispatcher.forward(request, response);
+
+
+        } else if (request.getParameter("edit-btn") != null) {
+
+
+        } else if (request.getParameter("delete-btn") != null) {
+
+        }
+
+
+    }
+}
