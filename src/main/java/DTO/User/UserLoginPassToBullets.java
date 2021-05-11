e5846f6a8decbd0327a5c83d1312260b169af4fa
Yasas Lowe <64888128+Flashminat0@users.noreply.github.com>
2021-05-11 12:07:28 +0530

    create new banner shows another pic i will change it later tho

--- src/main/java/DTO/User/UserLoginPassToBullets.java
+++ src/main/java/DTO/User/UserLoginPassToBullets.java
@@ -2,13 +2,13 @@ package DTO.User;
 
 public class UserLoginPassToBullets {
     public static String passwordTxtToBullets(String raw_password) {
-        String bulletpassword = "";
-        int lettersInbullets = raw_password.length();
+        String bulletPassword = "";
+        int lettersInBullets = raw_password.length();
         do {
-            lettersInbullets = lettersInbullets - 1;
-            bulletpassword = bulletpassword.concat("*");
-        } while (lettersInbullets > 0);
+            lettersInBullets = lettersInBullets - 1;
+            bulletPassword = bulletPassword.concat("*");
+        } while (lettersInBullets > 0);
 
-        return bulletpassword;
+        return bulletPassword;
     }
 }
