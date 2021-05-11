c4bd0ccf34861125907ecbf04877cffcdc018a40
Tharushi <tharushiishanka12@gmail.com>
2021-05-11 20:52:51 +0530

    created getters and setters of Description class

--- src/main/java/model/Description.java
+++ src/main/java/model/Description.java
@@ -1,4 +1,31 @@
 package model;
 
 public class Description {
+    private int movieID;
+    private String movieName;
+    private String movieDescription;
+
+    public void setMovieID(int movieID) {
+        this.movieID = movieID;
+    }
+
+    public int getMovieID() {
+        return movieID;
+    }
+
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
+
+    public String getMovieName() {
+        return movieName;
+    }
+
+    public void setMovieDescription(String movieDescription) {
+        this.movieDescription = movieDescription;
+    }
+
+    public String getMovieDescription() {
+        return movieDescription;
+    }
 }
